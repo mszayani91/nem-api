d351d74e26712ea0a750156f44291761c056feeb
mszayani91 <zayani.ms@gmail.com>
2022-02-22 20:33:12 +0100

    adding connection to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const routes = require("./routes/NemRoutes");
-const PORT = 8070;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+
+
+dotenv.config();
+const PORT = process.env.PORT || 8070;
 const app = express();
 
 app.use(routes);
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection established with MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 app.listen(PORT, async () => {
   console.log(`Nem API is running with the server up on port: ${PORT}`);
